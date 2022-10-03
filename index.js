8821cea0b35e6a08d5c8054d6237cf39134708f6
phchortolani <phchortolani@gmail.com>
2022-10-03 14:45:58 -0300

    initial express

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import data from "./scripts/geraTerritorio.js";
+import Express from "express";
 
+const app = Express()
+const port = process.env.PORT || 3000
 
-console.log(data)
+app.get('/', (req, res) => {
+    res.json(data)
+})
+
+app.listen(port, () => {
+    console.log(`server is running`)
+})
+
