3609090b257a40e0f360836f4471ec9eb7ad62e8
phchortolani <phchortolani@gmail.com>
2022-10-04 21:25:02 -0300

    add img for testing

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import data from "./public/scripts/geraTerritorio.js";
 import Express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = Express()
 const port = process.env.PORT || 3000
 
 app.use(Express.static('public'))
+
 app.use(Express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
     res.json(data)
 })
 
-app.get('/getTerritory', (req, res) => {
-    res.json(data)
+app.get('/getTerritory/:id', (req, res) => {
+    res.sendFile(__dirname + `/public/assets/${req.params.id}.png`);
 })
 
 app.listen(port, () => {
