a408b3cc49c487d802c4a888d1114ae0eec59943
phchortolani <phchortolani@gmail.com>
2023-02-02 15:54:35 -0300

    att

--- public/scripts/geraTerritorio.js
+++ public/scripts/geraTerritorio.js
@@ -11,13 +11,13 @@ const domLeaders = ["JOAO LIMA", "BRUNO", "ARNALDO", "VOLNEI"]
 const terLeaders = ["FERNANDO", "SEBASTIAO"]
 const sexLeaders = ["ARNALDO"]
 const quiLeaders = ["GERONIMO", "NATANAEL"]
-const sabLeader = "ALEX"
+const sabLeader = "VOLNEI"
 
 const currentDate = () => getCurrentDate();
 
-const UsarMenosTerritorios = true // caso seja false utilizara os mais antigos
+const UsarMenosTerritorios = false // caso seja false utilizara os mais antigos
 const devTest = false;
-const numMinCasas = 130
+const numMinCasas = 120
 /* const file = readFile('./sheet/CONTROLE_DE_TERRITORIO_2022.xlsx') */
 const file = readFile(devTest ? './sheet/teste.xlsx' : './sheet/CONTROLE_DE_TERRITORIO_2022.xlsx')
 
