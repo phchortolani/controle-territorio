93f88871991af186d761faf45c02589b74a0d7a6
phchortolaniLog <paulo.castro@logplanning.com.br>
2023-03-12 17:30:30 -0300

    att

--- public/scripts/geraTerritorio.js
+++ public/scripts/geraTerritorio.js
@@ -17,7 +17,7 @@ const currentDate = () => getCurrentDate();
 
 const UsarMenosTerritorios = false // caso seja false utilizara os mais antigos
 const devTest = false;
-const numMinCasas = 110
+const numMinCasas = 120
 const file = readFile(devTest ? './sheet/teste.xlsx' : './sheet/CONTROLE_DE_TERRITORIO_2022.xlsx')
 
 export function getData() {
